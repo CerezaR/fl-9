f5dba0d856c0eec3334bb6357a6334afd2f1e054
Yurii Korolkov <korolkow5@gmail.com>
2018-08-16 21:47:04 +0300

    using consle.log only once

--- FE_9_6_homework_js-intro/homework/src/js/task1.js
+++ FE_9_6_homework_js-intro/homework/src/js/task1.js
@@ -1,17 +1,24 @@
 let price = parseFloat(prompt('Enter amount of money', '0'));
 let discount = parseFloat(prompt('Enter discount', '0'));
+let output, rl_price, save;
 
 if (typeof price !== 'number' || typeof discount !== 'number') {
-	console.log('Invalid data')
+	output = 'Invalid data';
 } else {
 	if (price >= 0 && discount > 0 && discount <= 100) {
-		let rl_price = price - price * discount / 100;
-		let save = price - rl_price;
-		console.log(' Price without discount: ', +price.toFixed(2), '\n', 
-			'Discount: ', +discount.toFixed(2) ,'%', '\n', 
-			'Price with discount:', +rl_price.toFixed(2), '\n', 
-			'Saved:', +save.toFixed(2));
+		rl_price = price - price * discount / 100;
+		save = price - rl_price;
+		output = result();
 	} else {
-		console.log('Invalid data')
+		output = 'Invalid data';
 	}
 }
+
+function result() {
+	return `	Price without discount: ${+price.toFixed(2)} 
+	Discount: ${+discount.toFixed(2)}% 
+	Price with discount: ${+rl_price.toFixed(2)} 
+	Saved: ${+save.toFixed(2)}`;
+}
+
+console.log(output);
--- FE_9_6_homework_js-intro/homework/src/js/task2.js
+++ FE_9_6_homework_js-intro/homework/src/js/task2.js
@@ -1,23 +1,22 @@
 let length_1 = parseFloat(prompt('Enter lenght of first side', '0'));
 let length_2 = parseFloat(prompt('Enter discount', '0'));
 let angle = parseFloat(prompt('Enter angle between sides', '0'));
+let output, length_3, perimeter, square;
 
 if (typeof length_1 !== 'number' || typeof length_2 !== 'number' || typeof angle !== 'number') {
-	console.log('Invalid data')
+	output = 'Invalid data';
 } else {
 	if (length_1 > 0 && length_2 > 0 && angle > 0) {
-		let length_3 = getThirdSide();
-		let perimeter = getPerimeter(length_3);
-		let square = getSquare(perimeter, length_3);
-		console.log(' c length: ', +length_3.toFixed(2), '\n', 
-			'Triangle square: ', +square.toFixed(2), '\n', 
-			'Triangle perimeter: ', +perimeter.toFixed(2));
+		length_3 = getThirdSide();
+		perimeter = getPerimeter(length_3);
+		square = getSquare(perimeter, length_3);
+		output = result();
 	} else {
-		console.log('Invalid data')
+		output = 'Invalid data';
 	}
 }
 
-function getThirdSide(){
+function getThirdSide() {
 	const sumOfSides = 180;
 	let rad = Math.PI / sumOfSides * angle;
 	return Math.sqrt( Math.pow(length_1, 2) + Math.pow(length_2, 2) - 2 * length_1 * length_2 * Math.cos(rad));
@@ -32,3 +31,10 @@ function getSquare(perimeter, length_3) {
 	return Math.sqrt(hp * (hp - length_1) * (hp - length_2) * (hp - length_3));
 }
 
+function result() {
+	return `	c length: ${+length_3.toFixed(2)}
+	Triangle square: ${+square.toFixed(2)}
+	Triangle perimeter: ${+perimeter.toFixed(2)}`;
+}
+
+console.log(output);
