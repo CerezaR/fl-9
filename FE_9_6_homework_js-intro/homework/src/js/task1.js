912031ffd1f3daf74ac957119ba5e1210f27d75f
Yurii Korolkov <korolkow5@gmail.com>
2018-08-16 18:46:42 +0300

    changed const to let

--- FE_9_6_homework_js-intro/homework/src/js/task1.js
+++ FE_9_6_homework_js-intro/homework/src/js/task1.js
@@ -1,12 +1,12 @@
-const price = parseFloat(prompt('Enter amount of money', '0'));
-const discount = parseFloat(prompt('Enter discount', '0'));
+let price = parseFloat(prompt('Enter amount of money', '0'));
+let discount = parseFloat(prompt('Enter discount', '0'));
 
-if ( typeof price !== 'number' || typeof discount !== 'number' ) {
+if (typeof price !== 'number' || typeof discount !== 'number') {
 	console.log('Invalid data')
 } else {
-	if ( price >= 0 && discount > 0 && discount <= 100) {
-		const rl_price = price - price * discount / 100;
-		const save = price - rl_price;
+	if (price >= 0 && discount > 0 && discount <= 100) {
+		let rl_price = price - price * discount / 100;
+		let save = price - rl_price;
 		console.log(' Price without discount: ', +price.toFixed(2), '\n', 
 			'Discount: ', +discount.toFixed(2) ,'%', '\n', 
 			'Price with discount:', +rl_price.toFixed(2), '\n', 
