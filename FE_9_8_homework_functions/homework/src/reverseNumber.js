7256c21feef08f35c7463d1e41fe7ca68b818b46
Yurii Korolkov <korolkow5@gmail.com>
2018-08-22 20:50:46 +0300

    eighth task

---
+++ FE_9_8_homework_functions/homework/src/reverseNumber.js
@@ -0,0 +1,11 @@
+function reverseNumber(num) {
+	let reversed = 0;
+	let ten = 10;
+	while (num !== 0) {
+		reversed *= ten;
+		reversed += num % ten;
+		num -= num % ten;
+		num /= ten;
+	}
+	return reversed;
+}
